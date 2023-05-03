0dbc69e936cbc71f3be22e70bfedaa1350d75171
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-03 06:27:25 +0600

    primary login page added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
     {
       path: "/",
       errorElement:<ErrorPage> </ErrorPage>,
       element: <Main> </Main>,
+      children:[
+        {
+          path:'/login',
+          element:<Login> </Login>
+        },
+      ]
     },
   ]);
 
