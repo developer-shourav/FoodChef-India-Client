1679f2884771b0bb3e1435254f764b13b9322898
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-03 12:12:16 +0600

    Slider added

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -9,29 +9,129 @@ import "swiper/css/pagination";
 import "./Slider.css";
 
 // import required modules
-import { Pagination } from "swiper";
+import { Autoplay, Pagination } from "swiper";
+
+
+import SliderImg1 from '../../assets/slider-img/slider1.jpg'
+import SliderImg2 from '../../assets/slider-img/slider2.jpg'
+import SliderImg3 from '../../assets/slider-img/slider3.jpg'
 
 const Slider = () => {
     return (
-        <>
+        <div className="container mx-auto">
       <Swiper
-        pagination={{
-          dynamicBullets: true,
+        spaceBetween={30}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
         }}
-        modules={[Pagination]}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide> <div className='flex px-5 lg:px-0 flex-col-reverse md:flex-row my-4 md:my-8 lg:my-10 items-center justify-between'>
+               <div className='md:w-1/2 lg:ps-10'>
+                <h1 className='font-bold text-3xl md:text-5xl my-2 md:mb-5'> Chose Our Top </h1>
+                <h1 className='font-bold text-2xl  md:text-4xl lg:text-6xl mb-5'> Crockeries Here </h1>
+                
+                
+                <p className='md:w-3/4 text-gray-700 mb-5'>We stands for quality, authenticity, trust and satisfaction. We have a massive collection of products and brands that you will love.</p>
+                <button style={{background:'red'}} className='btn border-0 rounded-none px-8 '> Shop Now </button>
+
+                 <div className=' mt-5 md:mt-10 font-bold flex gap-20'>
+
+                   <div>
+                     <h3 className=' text-2xl md:text-3xl'>1534+</h3>
+                     <p ><small>Total Products</small></p>
+                   </div>
+
+                   <div>
+                   <h3 className=' text-2xl md:text-3xl'>1000+</h3>
+                     <p><small>Monthly Customers</small></p>
+                   </div>
+
+                 </div>
+               </div>
+
+               <div className='md:w-1/2'>
+                <img  src={SliderImg1} alt="" />
+               </div>
+            </div></SwiperSlide>
+
+
+
+
+        <SwiperSlide> <div className='flex px-5 lg:px-0 flex-col-reverse md:flex-row my-4 md:my-8 lg:my-10 items-center justify-between'>
+               <div className='md:w-1/2 lg:ps-10'>
+                <h1 className='font-bold text-3xl md:text-5xl my-2 md:mb-5'> Chose Our Top </h1>
+                <h1 className='font-bold text-2xl  md:text-4xl lg:text-6xl mb-5'> Crockeries Here </h1>
+                
+                
+                <p className='md:w-3/4 text-gray-700 mb-5'>We stands for quality, authenticity, trust and satisfaction. We have a massive collection of products and brands that you will love.</p>
+                <button style={{background:'red'}} className='btn border-0 rounded-none px-8 '> Shop Now </button>
+
+                 <div className=' mt-5 md:mt-10 font-bold flex gap-20'>
+
+                   <div>
+                     <h3 className=' text-2xl md:text-3xl'>1534+</h3>
+                     <p ><small>Total Products</small></p>
+                   </div>
+
+                   <div>
+                   <h3 className=' text-2xl md:text-3xl'>1000+</h3>
+                     <p><small>Monthly Customers</small></p>
+                   </div>
+
+                 </div>
+               </div>
+
+               <div className='md:w-1/2'>
+                <img  src={SliderImg2} alt="" />
+               </div>
+            </div>
+            </SwiperSlide>
+
+
+
+
+        
+        <SwiperSlide>
+        <div className='flex px-5 lg:px-0 flex-col-reverse md:flex-row my-4 md:my-8 lg:my-10 items-center justify-between'>
+               <div className='md:w-1/2 lg:ps-10'>
+                <h1 className='font-bold text-3xl md:text-5xl my-2 md:mb-5'> Chose Our Top </h1>
+                <h1 className='font-bold text-2xl  md:text-4xl lg:text-6xl mb-5'> Crockeries Here </h1>
+                
+                
+                <p className='md:w-3/4 text-gray-700 mb-5'>We stands for quality, authenticity, trust and satisfaction. We have a massive collection of products and brands that you will love.</p>
+                <button style={{background:'red'}} className='btn border-0 rounded-none px-8 '> Shop Now </button>
+
+                 <div className=' mt-5 md:mt-10 font-bold flex gap-20'>
+
+                   <div>
+                     <h3 className=' text-2xl md:text-3xl'>1534+</h3>
+                     <p ><small>Total Products</small></p>
+                   </div>
+
+                   <div>
+                   <h3 className=' text-2xl md:text-3xl'>1000+</h3>
+                     <p><small>Monthly Customers</small></p>
+                   </div>
+
+                 </div>
+               </div>
+
+               <div className='md:w-1/2'>
+                <img  src={SliderImg3} alt="" />
+               </div>
+            </div>
+        </SwiperSlide>
       </Swiper>
-    </>
+    </div>
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import Slider from '../../components/Slider/Slider';
 const Home = () => {
     return (
         <div>
-            <h1>This is Home</h1>
             <Slider> </Slider>
         </div>
     );
