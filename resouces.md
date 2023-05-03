bafce0da81a742db9ed2329761e6847040d75a79
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-03 14:37:17 +0600

    image resource added

---
+++ resouces.md
@@ -0,0 +1,12 @@
+
+### Chefs images
+
+1. https://i.ibb.co/Zd6yvT6/chef1.png
+2. https://i.ibb.co/tDfKSYL/chef2.png
+3. https://i.ibb.co/fXZ9WC6/chef3.png
+4. https://i.ibb.co/q0g3Z7X/chef4.png
+5. https://i.ibb.co/KNj0tyW/chef5.webp
+6. https://i.ibb.co/nCn4FTP/chef6.png
+7. https://i.ibb.co/3BVTz1P/chef7.png
+8. https://i.ibb.co/pnGRMs3/chef8.png
+9. https://i.ibb.co/jhmf07M/chef9.webp
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import Slider from '../../components/Slider/Slider';
+import React from "react";
+import Slider from "../../components/Slider/Slider";
+
 
 const Home = () => {
-    return (
+  return (
+    <div>
+      {/*------------Top Banner---------- */}
+      <Slider> </Slider>
+
+      {/* --------Chefs Cards ----------*/}
+
+      <div className="container mx-auto">
+        <h2>Find Your Chef</h2>
         <div>
-     {/*------------Top Banner---------- */}
-            <Slider> </Slider>
-
-
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
